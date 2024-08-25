ed851ee470c1ae06a21095d98f1bd8d8d2398c97
Mutta885 <mutta885@gmail.com>
2024-08-25 03:21:55 +0300

    start project

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,11 +1,12 @@
 package com.example.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+
 data class Track (
     val trackName: String,      // Название композиции
     val artistName: String,     // Имя исполнителя
-    val trackTime: String,      // Продолжительность трека
+    @SerializedName("trackTimeMillis")
+    val trackTime: Long,      // Продолжительность трека
     val artworkUrl100: String   // Ссылка на изображение обложки
 
 )
-
-
